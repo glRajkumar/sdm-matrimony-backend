01304a032eaf1e7380a8b6d4e65a406ff302e12b
Raj kumar <glrajv369@gmail.com>
2025-04-22 18:22:40 +0530

    ollama extractor testing

--- src/controllers/extractor.ts
+++ src/controllers/extractor.ts
@@ -1,16 +1,43 @@
 import type { Context } from "hono";
+import ollama from 'ollama';
 
-import { saveImageLocally } from "../utils/index.js";
+import { getImgUrl, saveImageLocally } from "../utils/index.js";
 
 export async function extractImg(c: Context) {
   const formData = await c.req.formData()
 
-  const image = formData.get("image")
+  const images = formData.getAll("images")
 
-  if (!image) return c.json({ message: 'No images found' }, 400)
+  if (!images || (Array.isArray(images) && images.length === 0)) return c.json({ message: 'No images found' }, 400)
 
-  const url = await saveImageLocally(image)
+  const [detailImg, ...rest] = images
+  const imagePath = await saveImageLocally(detailImg)
 
-  return c.json({ url })
+  const response = await ollama.chat({
+    model: 'llava:7b',
+    messages: [
+      {
+        role: 'system',
+        content: `
+  You are a Tamil OCR and data extraction expert. The input will be an image of a biodata/matrimony profile in Tamil language. 
 
+  Use the following fields:
+  இனம், பெயர், பிறந்ததேதி / நேரம், பிறந்த ஊர், நட்சத்திரம், ராசி, லக்னம், கல்வி தகுதி, வேலை / சம்பளம், தந்தை பெயர், தாயார் பெயர், உடன்பிறந்த ஆண்கள், உடன்பிறந்த பெண்கள், பிறப்புவரிசை, இருப்பிடம்  
+
+  Your task:
+  - Extract all relevant data from the image.
+  - Do not add any explanation, notes, or markdown. Only return raw extracted data in JSON format.
+        `
+      },
+      {
+        role: 'user',
+        content: 'Extract biodata and return in JSON',
+        images: [imagePath]
+      }
+    ],
+  })
+
+  const imagePaths = await Promise.all(rest.map(async (image) => await getImgUrl(image)))
+
+  return c.json({ imagePaths, extracted: response.message.content })
 }
