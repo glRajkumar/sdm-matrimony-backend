6aadc5740c12e935d4a8f7a1c63c31dacca4a71a
Raj kumar <glrajv369@gmail.com>
2025-04-23 17:49:37 +0530

    hided data extraction from ollama

--- src/controllers/extractor.ts
+++ src/controllers/extractor.ts
@@ -1,5 +1,5 @@
 import type { Context } from "hono";
-import ollama from 'ollama';
+// import ollama from 'ollama';
 
 import { getImgUrl, saveImageLocally } from "../utils/index.js";
 
@@ -10,34 +10,34 @@ export async function extractImg(c: Context) {
 
   if (!images || (Array.isArray(images) && images.length === 0)) return c.json({ message: 'No images found' }, 400)
 
-  const [detailImg, ...rest] = images
-  const imagePath = await saveImageLocally(detailImg)
+  // const [detailImg, ...rest] = images
+  // const imagePath = await saveImageLocally(detailImg)
 
-  const response = await ollama.chat({
-    model: 'llava:7b',
-    messages: [
-      {
-        role: 'system',
-        content: `
-  You are a Tamil OCR and data extraction expert. The input will be an image of a biodata/matrimony profile in Tamil language. 
+  // const response = await ollama.chat({
+  //   model: 'llava:7b',
+  //   messages: [
+  //     {
+  //       role: 'system',
+  //       content: `
+  // You are a Tamil OCR and data extraction expert. The input will be an image of a biodata/matrimony profile in Tamil language. 
 
-  Use the following fields:
-  இனம், பெயர், பிறந்ததேதி / நேரம், பிறந்த ஊர், நட்சத்திரம், ராசி, லக்னம், கல்வி தகுதி, வேலை / சம்பளம், தந்தை பெயர், தாயார் பெயர், உடன்பிறந்த ஆண்கள், உடன்பிறந்த பெண்கள், பிறப்புவரிசை, இருப்பிடம்  
+  // Use the following fields:
+  // இனம், பெயர், பிறந்ததேதி / நேரம், பிறந்த ஊர், நட்சத்திரம், ராசி, லக்னம், கல்வி தகுதி, வேலை / சம்பளம், தந்தை பெயர், தாயார் பெயர், உடன்பிறந்த ஆண்கள், உடன்பிறந்த பெண்கள், பிறப்புவரிசை, இருப்பிடம்  
 
-  Your task:
-  - Extract all relevant data from the image.
-  - Do not add any explanation, notes, or markdown. Only return raw extracted data in JSON format.
-        `
-      },
-      {
-        role: 'user',
-        content: 'Extract biodata and return in JSON',
-        images: [imagePath]
-      }
-    ],
-  })
+  // Your task:
+  // - Extract all relevant data from the image.
+  // - Do not add any explanation, notes, or markdown. Only return raw extracted data in JSON format.
+  //       `
+  //     },
+  //     {
+  //       role: 'user',
+  //       content: 'Extract biodata and return in JSON',
+  //       images: [imagePath]
+  //     }
+  //   ],
+  // })
 
-  const imagePaths = await Promise.all(rest.map(async (image) => await getImgUrl(image)))
+  const imagePaths = await Promise.all(images.map(async (image) => await getImgUrl(image)))
 
-  return c.json({ imagePaths, extracted: response.message.content })
+  return c.json({ imagePaths }) // extracted: response.message.content
 }
