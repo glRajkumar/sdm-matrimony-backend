f2c29aa3fd37dc7c1597132118a15349fc45dad4
Raj kumar <glrajv369@gmail.com>
2025-10-22 19:08:57 +0530

    update: port

--- src/index.ts
+++ src/index.ts
@@ -88,7 +88,7 @@ app.onError((err, c) => {
   return c.json({ message: err?.message || "Internal sever eror" }, 500)
 })
 
-const port = Number(process.env.PORT || 5000)
+const port = Number(process.env.PORT || 5001)
 console.log(`Server is running on http://localhost:${port}`)
 
 serve({
